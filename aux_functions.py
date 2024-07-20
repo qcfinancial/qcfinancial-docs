56f4a3e3c283cb5adb17264c8681da77fa10efac
Alvaro Diaz Valenzuela <alvaro@efaa.cl>
2024-07-20 17:42:24 -0400

    Mejoras a los NBs 1 y 2

--- aux_functions.py
+++ aux_functions.py
@@ -1,17 +1,6 @@
 import qcfinancial as qcf
+import holidays as hol
 import pandas as pd
-
-def leg_as_dataframe(leg: qcf.Leg):
-    """
-    Envuelve un objeto qcf.Leg en un pd.DataFrame
-    """
-    if leg.size() == 0:
-        raise ValueError("No cashflows")
-    type_cashflows = leg.get_cashflow_at(0).get_type()
-    tabla = [qcf.show(leg.get_cashflow_at(i)) for i in range(0, leg.size())]
-    df = pd.DataFrame(tabla, columns=qcf.get_column_names(type_cashflows, ''))
-    return df
-
 
 format_dict = {
     'nominal': '{0:,.2f}', 
@@ -35,3 +24,23 @@ format_dict = {
     'interes_moneda_pago': '{0:,.2f}', 
     'valor_indice_fx': '{0:,.2f}'
 }
+
+def leg_as_dataframe(leg: qcf.Leg):
+    """
+    Envuelve un objeto qcf.Leg en un pd.DataFrame
+    """
+    if leg.size() == 0:
+        raise ValueError("No cashflows")
+    type_cashflows = leg.get_cashflow_at(0).get_type()
+    tabla = [qcf.show(leg.get_cashflow_at(i)) for i in range(0, leg.size())]
+    df = pd.DataFrame(tabla, columns=qcf.get_column_names(type_cashflows, ''))
+    return df
+
+
+def get_business_calendar(which_holidays: str, years: range) -> qcf.BusinessCalendar:
+    py_cal = hol.country_holidays(which_holidays, years=years)
+    yrs = [y for y in years]
+    qcf_cal = qcf.BusinessCalendar(qcf.QCDate(1, 1, yrs[0]), yrs[-1] - yrs[0])
+    for d in py_cal.keys():
+        qcf_cal.add_holiday(qcf.QCDate(d.isoformat()))
+    return qcf_cal
